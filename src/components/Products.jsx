28fa77c548d9b6f5f12ae5d341f8b0bcb89380c1
nazreen <nazreenshaikh0077@gmail.com>
2022-04-09 23:43:02 +0530

    all require components

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { addToCart } from '../store/cartSlice'
+import { useDispatch } from 'react-redux'
+import Loading from './Loading'
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+  const handleAdd = (product) => {
+    dispatch(addToCart(product))
+  }
   useEffect(() => {
     const fetchProducts = async () => {
       await fetch('https://fakestoreapi.com/products')
@@ -10,11 +19,34 @@ const Products = () => {
         .then((data) => {
           console.log(data)
           setProducts(data)
+          setLoading(false)
         })
     }
     fetchProducts()
   }, [])
-  return <div>Products</div>
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="productsWrapper">
+          {products.map((product) => {
+            return (
+              <div className="card" key={product.id}>
+                <h1>{product.name}</h1>
+                <img src={product.image} alt={product.title} />
+                <p>{product.title}</p>
+                <p>{product.price} $</p>
+                <button onClick={() => handleAdd(product)} className="btn">
+                  Add to cart
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Products
